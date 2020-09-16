9d33be264c3d9ef7df4c50f93eb8dcbc68fc42c3
xxdxxs <1040969744@qq.com>
2020-09-16 15:12:28 +0800

    修改pom.xml,配置上传到maven中央仓库

--- src/main/java/com/xxdxxs/utils/TimeUtil.java
+++ src/main/java/com/xxdxxs/utils/TimeUtil.java
@@ -1,4 +1,32 @@
 package com.xxdxxs.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class TimeUtil {
+
+    /**
+     * 获取该月最后一天
+     * @param month
+     */
+    public static String getLastDayOfMonth(int month) {
+        Calendar cal = Calendar.getInstance();
+        // 设置月份
+        cal.set(Calendar.MONTH, month - 1);
+        // 获取某月最大天数
+        int lastDay=0;
+        //2月的平年瑞年天数
+        if(month==2) {
+            lastDay = cal.getLeastMaximum(Calendar.DAY_OF_MONTH);
+        }else {
+            lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        }
+        // 设置日历中月份的最大天数
+        cal.set(Calendar.DAY_OF_MONTH, lastDay);
+        // 格式化日期
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String lastDayOfMonth = sdf.format(cal.getTime());
+        return lastDayOfMonth;
+
+    }
 }
