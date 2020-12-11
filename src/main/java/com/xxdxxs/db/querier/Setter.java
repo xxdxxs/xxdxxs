a51b92fcdeb477b3e9651b9b19700f6facfd8d6a
xxdxxs <1040969744@qq.com>
2020-12-11 14:20:15 +0800

    字段值设置接口

--- src/main/java/com/xxdxxs/db/querier/Setter.java
+++ src/main/java/com/xxdxxs/db/querier/Setter.java
@@ -1,11 +1,15 @@
 package com.xxdxxs.db.querier;
 
-import com.xxdxxs.db.jdbc.Column;
+import com.xxdxxs.support.Column;
 import org.springframework.util.ObjectUtils;
 
 import java.util.Collection;
 import java.util.Set;
 
+/**
+ * 字段值设置接口
+ * @author xxdxxs
+ */
 public interface Setter<R, V extends Column> {
 
     R set(V variable);
