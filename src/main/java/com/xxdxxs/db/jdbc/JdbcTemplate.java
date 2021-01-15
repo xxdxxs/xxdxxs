3ac31cab705b73b88c041f7c77d8d64ca6e079d6
xxdxxs <1040969744@qq.com>
2021-01-15 17:58:06 +0800

    新增JdbcTemplate 继承了spring JdbcTemplate

---
+++ src/main/java/com/xxdxxs/db/jdbc/JdbcTemplate.java
@@ -0,0 +1,39 @@
+package com.xxdxxs.db.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.xxdxxs.db.dao.ConnectInfo;
+import com.xxdxxs.db.dao.Template;
+import org.springframework.util.Assert;
+import javax.sql.DataSource;
+
+/**
+ * @author xxdxxs
+ */
+public class JdbcTemplate extends org.springframework.jdbc.core.JdbcTemplate implements Template {
+
+
+    public static JdbcTemplate of(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    public static JdbcTemplate of(ConnectInfo connectInfo) {
+        Assert.notNull(connectInfo, "connectInfo must not be null");
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(connectInfo.getDriverClassName());
+        dataSource.setUrl(connectInfo.getUrl());
+        dataSource.setUsername(connectInfo.getUsername());
+        dataSource.setPassword(connectInfo.getPassword());
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(600000);
+        dataSource.setMaxActive(connectInfo.getMaxActive() == null? 20 : connectInfo.getMaxActive());
+        dataSource.setMinIdle(connectInfo.getMinIdle() == null? 1 : connectInfo.getMinIdle());
+        dataSource.setInitialSize(connectInfo.getInitialSize() == null? 1 : connectInfo.getInitialSize());
+        return of(dataSource);
+    }
+
+}
