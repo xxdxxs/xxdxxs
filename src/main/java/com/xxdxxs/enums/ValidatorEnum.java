f1de0cf94b7796f07b35959bc2643ad83e021009
xxdxxs <1040969744@qq.com>
2021-01-19 09:54:42 +0800

    新增校验异常信息

--- src/main/java/com/xxdxxs/enums/ValidatorEnum.java
+++ src/main/java/com/xxdxxs/enums/ValidatorEnum.java
@@ -5,7 +5,8 @@ public enum ValidatorEnum {
     PARAM_IS_VAILD("100002", "参数无效"),
     ERROR_FORMAT("100003", "参数格式不正确"),
     OUt_LIMIT("100004", "超出范围"),
-    ERROE_GET_EXCEPTION("100005", "获取异常信息出错");
+    ERROE_GET_EXCEPTION("100005", "获取异常信息出错"),
+    PARAM_NOT_IN_RANGE("100006", "参数值不在指定范围内"),;
 
     private String code;
 
