8261210de662918495ed4ebd380cc31905253e8d
xxdxxs <1040969744@qq.com>
2021-01-26 16:50:21 +0800

    完善转换

--- src/main/java/com/xxdxxs/support/conversion/IntegerConversion.java
+++ src/main/java/com/xxdxxs/support/conversion/IntegerConversion.java
@@ -1,5 +1,7 @@
 package com.xxdxxs.support.conversion;
 
+import com.xxdxxs.validation.Validation;
+
 import java.util.Optional;
 
 /**
@@ -9,6 +11,38 @@ public class IntegerConversion implements Conversion<Integer> {
 
     @Override
     public Optional<Integer> convert(Object value) {
-        return Optional.empty();
+        if (Validation.isEmpty(value)) {
+            return Optional.empty();
+        }
+
+        if (value instanceof Integer) {
+            return Optional.of((Integer) value);
+        }
+
+        if (Validation.isClass(value, int.class)) {
+            return Optional.of(Integer.valueOf((int) value));
+        }
+
+        if (value instanceof String) {
+            return convert((String) value);
+        }
+
+        if (value instanceof Number) {
+            return Optional.of(((Number) value).intValue());
+        }
+
+        return convert(String.valueOf(value));
     }
+
+
+    public Optional<Integer> convert(String value) {
+        try {
+            return Optional.of(Integer.parseInt(value));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+
+
 }
